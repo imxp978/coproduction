562e626a1342c1c6b1504ef1f90587331ffb790a
imxp978 <103916953+imxp978@users.noreply.github.com>
2024-05-21 17:46:18 +0800

    init2

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
+import Modal from "./components/Modal.vue";
+import Footer from "./components/Footer.vue";
+
+
+
+// import {useCategoryStore} from "@/stores/CategoryStore";
+
+// const categoryStore = useCategoryStore();
+
+</script>
+
+<template>
+  <Navbar />
+  <Modal />
+  <RouterView />
+  <Footer />
+</template>
+
